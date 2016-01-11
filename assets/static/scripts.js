5e99a1f54d39bf5676708f2d5b5b796e2768d230
Jan Heuermann <jan@jotaen.net>
2016-01-11 22:52:15 +0100

    Animate menu icon labels

--- assets/static/scripts.js
+++ assets/static/scripts.js
@@ -5,14 +5,32 @@ var add_sidebar_gloss = function(node, dest) {
   dest.insertBefore(node, dest.firstChild);
 };
 
-(function create_sidebar_gloss_from_footnote() {
-  var refs = document.getElementsByClassName("footnote");
-  [].forEach.call(refs, function(node, i) {
+(function create_sidebar_glosses_from_footnotes() {
+  var footnotes = document.getElementsByClassName("footnote");
+  [].forEach.call(footnotes, function(_, i) {
     var nr = i+1;
     var content = document.getElementById("fn:"+nr);
     var dest = document.getElementById("fnref:"+nr);
-    var clone = content.cloneNode(true).firstElementChild;
+    var clone = content.cloneNode(true);
 
-    add_sidebar_gloss(clone, dest);
+    add_sidebar_gloss(clone.firstElementChild, dest);
+  });
+}());
+
+var show_label = function() {
+  var label = this.getElementsByClassName("icon__label")[0];
+  label.style.display = "block";
+};
+
+var hide_label = function() {
+  var label = this.getElementsByClassName("icon__label")[0];
+  label.style.display = "none";
+};
+
+(function add_event_listeners_to_menu_icons() {
+  var icons = document.getElementsByClassName("icon--navi");
+  [].forEach.call(icons, function(icon, i) {
+    icon.addEventListener("mouseover", show_label);
+    icon.addEventListener("mouseout",  hide_label);
   });
 }());
