a2de8240ddcf58f47d5ad47777c62a9999af9eff
Jan Heuermann <jan@jotaen.net>
2017-12-27 11:13:24 +0100

    Upgrade to latest hugo version

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run --rm -v $(pwd):/app -p 1313:1313 -w /app publysher/hugo hugo server --bind=0.0.0.0 --buildDrafts
+docker run --rm -v $(pwd):/app -p 1313:1313 -w /app jojomi/hugo hugo server --bind=0.0.0.0 --buildDrafts
